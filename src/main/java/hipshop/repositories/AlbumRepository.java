2063bc3975adf08d44b591da7afbf9c427bfcaa2
Tania Gayosso <taniagd09@gmail.com>
2021-08-08 12:54:12 -0500

    Creación de services y controllers de album y product

--- src/main/java/hipshop/repositories/AlbumRepository.java
+++ src/main/java/hipshop/repositories/AlbumRepository.java
@@ -1,9 +1,15 @@
 package hipshop.repositories;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import hipshop.models.Album;
 
+@Repository
 public interface AlbumRepository extends CrudRepository<Album, Long> {
+
+	public abstract ArrayList<Album> findByproductNameContaining(String productName);
 
 }
